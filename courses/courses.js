1a3fe47bfb0c27dfde5d5b066771c153e7512375
drewsteele408 <drewsteele408@gmail.com>
2025-02-26 09:45:03 -0700

    courses page

---
+++ courses/courses.js
@@ -0,0 +1,7 @@
+import {aCourse} from "./course.mjs";
+
+
+document.querySelector("#enrollStudent").addEventListener("click", function () {
+  const sectionNum = document.querySelector("#sectionNumber").value;
+  aCourse.enrollStudent(sectionNum);
+  });

6952d760753ca05eed6b69ecc9f8a1a6db7680fa
drewsteele408 <drewsteele408@gmail.com>
2025-03-22 22:28:03 -0600

    site plan

---
+++ target.js
@@ -0,0 +1,3 @@
+document.getElementById('intro').style.backgroundColor = 'f2c556';
+document.querySelector('em').style.backgroundColor = 'acbfcc';
+document.querySelector('em').textContent = 'USS Voyager Starship';
